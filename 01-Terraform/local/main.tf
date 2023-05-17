07c81c418cca4d61bd47641220e2bd4f4844ce0a
yellowpenguincookie <yurijjjung@gmail.com>
2023-05-17 16:07:18 +0900

    20230517

--- 01-Terraform/local/main.tf
+++ 01-Terraform/local/main.tf
@@ -5,6 +5,17 @@ provider "local" {
 #infra resource
 
 resource "local_file" "foo" {
+#출력파일 지정
   filename = "${path.module}/foo.txt"
-  content  = "Hello World!"
+  content  = data.local_file.bar.content
+}
+
+#입력 데이터파일 지정
+data "local_file" "bar" {
+  filename = "${path.module}/bar.txt"
+}
+
+#데이터 출력 지정
+output "file_bar" {
+  value = data.local_file.bar
 }
